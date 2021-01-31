ab02f08981e942763a13c5c74a833e2c315e7fb4
Velo <hartley.max@outlook.com>
2021-01-31 10:13:55 +0000

    nav button smoother transitions implemented

--- components/project/projectButtons.jsx
+++ components/project/projectButtons.jsx
@@ -1,14 +1,29 @@
+import { useState } from 'react';
+
 export default function ProjectButton({ isSelected }) {
-	return isSelected ? (
-		<svg xmlns='http://www.w3.org/2000/svg' width='22' height='22'>
-			<g stroke='#fc5185' strokeWidth='3' fill='none'>
-				<circle cx='11' cy='11' r='11' stroke='none' />
-				<circle cx='11' cy='11' r='9.5' />
-			</g>
-		</svg>
-	) : (
-		<svg xmlns='http://www.w3.org/2000/svg' width='14' height='14'>
-			<circle cx='7' cy='7' r='7' fill='#fc5185' />
+	const [isHovered, setIsHovered] = useState(false);
+	return (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			width={isSelected ? '22' : '14'}
+			height={isSelected ? '22' : '14'}
+			style={{
+				transition: 'all 0.15s, transform 0.3s',
+				margin: !isSelected ? '7px' : '3px',
+				transform: `scale(${isSelected ? 1 : isHovered ? 1.5 : 1})`,
+			}}
+			onMouseEnter={() => setIsHovered(true)}
+			onMouseLeave={() => setIsHovered(false)}
+		>
+			<circle
+				style={{ transition: '0.15s' }}
+				cx={isSelected ? '11' : '7'}
+				cy={isSelected ? '11' : '7'}
+				r={isSelected ? '9.5' : '7'}
+				fill={isSelected ? '#fc518500' : '#fc5185'}
+				strokeWidth={isSelected ? '3' : '0'}
+				stroke='#fc5185'
+			/>
 		</svg>
 	);
 }
