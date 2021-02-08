6417b9da26d58cb2a8c98c20861e7fc7dafaf75e
Velo <hartley.max@outlook.com>
2021-02-08 16:03:22 +0000

    bars background removed

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,6 @@
 import Head from 'next/head';
 import style from '../styles/index.module.css';
 import Link from 'next/link';
-import Bars from '../components/bars/bars';
 
 export default function Home() {
 	return (
@@ -124,7 +123,6 @@ export default function Home() {
 				<Link href='/portfolio'>
 					<a className={style.portfolioLink}>Click to view my work</a>
 				</Link>
-				<Bars />
 			</main>
 		</>
 	);
