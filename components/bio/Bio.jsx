f07ed74cb5aca6c25cedbdf97da6014f657af52b
Velo <hartley.max@outlook.com>
2021-03-04 13:16:48 +0000

    bio paragraph updated

--- components/bio/Bio.jsx
+++ components/bio/Bio.jsx
@@ -44,14 +44,13 @@ export default function Bio({ isHidden, onClick, top, left }) {
 			<p className={styles.speechBubble} style={bubbleStyle}>
 				I am a self taught Full Stack Web Developer with a passion for creating bespoke,
 				elegant websites. In 2017 I graduated from the University of Birmingham with a
-				Bachelor's Degree in Mathematics. Shortly afterwards, my coding journey began
-				through the
+				Bachelor's Degree in Mathematics. My coding journey began shortly after through the
 				<a href='https://www.codecademy.com/profiles/Velocimactor' target='_blank'>
 					{' '}
 					Codecademy{' '}
 				</a>{' '}
-				Web Development course where I found fulfilment in the flow coding. Since then my
-				skill set has expanded and I have created professional websites for several local
+				Web Development course where I found fulfilment in the flow of coding. Since then I
+				have expanded my skill set and created professional websites for several local
 				businesses. In the future I hope to advance my knowledge enough to contribute to new
 				and exciting technologies. For examples of my work, check out my{' '}
 				<Link href='/portfolio'>
