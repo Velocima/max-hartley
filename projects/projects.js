bb68d477559a16c234587c4ace1f805fe6c35200
Velo <hartley.max@outlook.com>
2021-08-12 10:50:07 +0100

    update portfolio projects

--- projects/projects.js
+++ projects/projects.js
@@ -13,6 +13,13 @@ const projectInfo = [
 		github: 'https://github.com/Velocima/green-lawns-next',
 	},
 	{
+		image: '/projects/habitude.jpg',
+		title: 'Habitude',
+		year: '2021',
+		link: 'https://habitudeapp.netlify.app/',
+		github: 'https://github.com/the-simian-army/habit-tracker',
+	},
+	{
 		image: '/projects/portfolio.jpg',
 		title: 'Personal Portfolio',
 		year: '2021',
@@ -20,18 +27,32 @@ const projectInfo = [
 		github: 'https://github.com/Velocima/max-hartley',
 	},
 	{
+		image: '/projects/repo-tracker.jpg',
+		title: 'Repository Tracker',
+		year: '2021',
+		link: 'https://repo-tracker.netlify.app/',
+		github: 'https://github.com/james-max-futureproof/repo-tracker',
+	},
+	{
+		image: '/projects/memoriae.jpg',
+		title: 'Memoriaes',
+		year: '2021',
+		link: 'https://memoriaes.netlify.app/',
+		github: 'https://github.com/max-monica/memoriae',
+	},
+	{
+		image: '/projects/journel.jpg',
+		title: 'Journel',
+		year: '2021',
+		link: 'https://journell.netlify.app/',
+		github: 'https://github.com/max-hartley/ram-fp-journel-project',
+	},
+	{
 		image: '/projects/french-au-cafe.png',
 		title: 'French Au Café',
 		year: '2020',
 		link: 'https://frenchaucafe.com/',
 		github: 'https://github.com/Velocima/ginnie-french',
-	},
-	{
-		image: '/projects/millicent.jpg',
-		title: 'Millicent Mehndi',
-		year: '2020',
-		link: 'http://www.millicent-mehndi.co.uk/',
-		github: 'https://github.com/Velocima/millie-next',
 	},
 ];
 
